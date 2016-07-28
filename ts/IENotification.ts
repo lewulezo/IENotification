b9abf909f959dc0b7f71d9ad82e01077473eb331
lewulezo <lewulezo@gmail.com>
2016-07-28 07:05:03 +0000

    ...

--- ts/IENotification.ts
+++ ts/IENotification.ts
@@ -1,6 +1,7 @@
 import Position from './Position';
 import Observable from './Observable';
 import DelayTasks from './DelayTasks';
+import IENotificationQueue from './IENotificationQueue';
 
 declare interface IWindow extends Window{
   open(url?: string, target?: string, features?: string, replace?: boolean): IWindow;
@@ -265,45 +266,7 @@ function numberToPx(num:number):string{
   return num + 'px';
 }
 
-module IENotificationQueue{
-  let maxQueueSize = 20;
-  let popupQueue:IENotification[] = [];
-  let currentNoti:IENotification;
 
-  export function add(noti:IENotification){
-    if (popupQueue.length > maxQueueSize){
-      return;
-    }
-    noti.on(EVENT_OPEN, ()=>currentNoti = noti);
-    noti.on(EVENT_DISPOSE, ()=>{
-      currentNoti = null;
-      remove(noti);
-    });
-    if (isEmpty() && !currentNoti){
-      noti.show();
-    } else {
-      popupQueue.push(noti);
-    }
-  }
-
-  function remove(noti:IENotification){
-    arrayRemove(popupQueue, noti);
-    if (!isEmpty()){
-      window.setTimeout(() => popupQueue.pop().show(), 200);
-    }
-  }
-
-  function isEmpty():boolean{
-    return popupQueue.length == 0;
-  }
-
-  function arrayRemove(arr:Array<any>, item){
-    let index = arr.indexOf(item);
-    if (index > -1){
-      arr.splice(index, 1);
-    }
-  }
-}
 
 function syncWindowPosition(targetWin:Window, refWin:Window, offset={x:0,y:0}){
   try {
