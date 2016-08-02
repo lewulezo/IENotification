f59bd170f2fd26d7377650a56d7e3a9fddaf1f91
Yudan Lin <yudan.lin@hpe.com>
2016-08-02 11:16:57 +0800

    force close message after timeout in notification queue

--- ts/IENotification.ts
+++ ts/IENotification.ts
@@ -24,7 +24,7 @@ export class IENotification extends Observable{
   private _popup: IDialog;
   private _bridge: Window;
   private _delayTasks: DelayTasks;
-  protected closed: boolean;
+  closed: boolean;
 
   public static timeout = 20000;
   public static basePath = '';
